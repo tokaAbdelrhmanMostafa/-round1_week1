c0352843e6e4615e0a6c1829dabf087dc6675170
TokaAbdelrhman <126849459+tokaAbdelrhmanMostafa@users.noreply.github.com>
2023-03-15 07:17:59 +0200

    Add files via upload

---
+++ B. Basic Data Types.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	int num;
+	float num2;
+	long long num3;
+	char ch;
+  double d;
+  cin >>num >> num3>> ch>> d >> num2;
+   cout<< num<<endl;
+  cout<< num3<<endl;
+  cout<< ch<<endl;
+  cout<< d<<endl;
+  cout<< num2<<endl; 
+}
